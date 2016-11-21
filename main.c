49993d037bfd4b77d41e566a3ee4fef7bf87764c
Aeurum Eaxon <aeurum@gmail.com>
2016-11-21 21:48:23 +0300

    Create main.c
    I have no idea what I’m doing!

---
+++ main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <inttypes.h>
+
+uint64_t cats(int c)
+{
+    int i;
+    uint64_t n = 0;
+    
+    for (i = 0; i < c; i++) {
+        n = ++n * ( i + 1 );
+    }
+    
+    return n;
+}
+
+int main()
+{
+    int i;
+    
+    for (i = 0; i < 16; i++) {
+        printf("%d => %16"PRId64"\n", i, cats(i));
+    }
+
+    return 0;
+}
