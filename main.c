85c9888ea40e99967311dc56cfaf07c221bd979c
Aeurum Eaxon <aeurum@gmail.com>
2017-01-04 16:51:33 +0300

    Update main.c

--- main.c
+++ main.c
@@ -6,21 +6,17 @@ uint64_t cats(int c)
 {
     int i;
     uint64_t n = 0;
-    
     for (i = 0; i < c; i++) {
-        n = ++n * ( i + 1 );
-    }
-    
+        n = ++n * (i + 1);
+    }   
     return n;
 }
 
 int main()
 {
     int i;
-    
     for (i = 0; i < 16; i++) {
         printf("%d => %16" PRIu64 "\n", i, cats(i));
     }
-
     return 0;
 }
